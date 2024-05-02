fe546605d7455313d27579383bc4808aebff3a70
AI-chatterbot-v2 <chivachrin@gmail.com>
2024-05-02 10:58:39 +0700

    add some file

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import cv2
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Loading file")
+# Load the pre-trained Haar Cascade model for face detection
+base_dir = os.path.dirname(cv2.__file__)
+xml_path = os.path.join(base_dir, 'data', 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(xml_path)
 
+# Load the image where you want to detect faces
+image = cv2.imread('images/image1.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'vava, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Detect faces in the image
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
 
+# Draw rectangles around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Save or display the result
+cv2.imwrite('faces_detected.jpg', image)
+# If you want to display the result using OpenCV's GUI window uncomment the lines below
+cv2.imshow('Faces Detected', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
